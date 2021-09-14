5c1f72fb422fdd226d8aba1a60127014d9623f53
Trevor Sears <trevors@knorrmarketing.com>
2021-09-14 15:05:23 -0400

    Updated copyright headers.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 /*
- *	Created by Trevor Sears <trevorsears.main@gmail.com>.
- *	8:48 PM -- June 16th, 2019.
- *	Project: @jsdsl/bimap
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 8:48 PM -- June 16th, 2019.
+ * Project: @jsdsl/bimap
+ * 
+ * @jsdsl/bimap - A bidirectional map written in TypeScript.
+ * Copyright (C) 2021 Trevor Sears
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 const gulp = require("gulp");
--- ts/bi-map.ts
+++ ts/bi-map.ts
@@ -1,14 +1,30 @@
 /*
- *	Created by Trevor Sears <trevorsears.main@gmail.com>.
- *	1:04 PM -- September 14th, 2019.
- *	Project: @jsdsl/bimap
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 1:04 PM -- September 14th, 2019.
+ * Project: @jsdsl/bimap
+ * 
+ * @jsdsl/bimap - A bidirectional map written in TypeScript.
+ * Copyright (C) 2021 Trevor Sears
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /**
- * A bidirectional map.
+ * A bidirectional map written in TypeScript.
  *
  * @author Trevor Sears <trevorsears.main@gmail.com>
- * @version v0.1.0
+ * @version v1.0.0
  * @since v0.1.0
  */
 export class BiMap<K = any, V = any> {
